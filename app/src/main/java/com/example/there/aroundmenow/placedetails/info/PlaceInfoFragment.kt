4b75e15ea4cccad03be38649666f775d53ad51f6
Tomasz Osoba <tomasz.osoba@comarch.pl>
2019-01-02 16:28:29 +0100

    PlaceInfo types chips.

--- app/src/main/java/com/example/there/aroundmenow/placedetails/info/PlaceInfoFragment.kt
+++ app/src/main/java/com/example/there/aroundmenow/placedetails/info/PlaceInfoFragment.kt
@@ -1,9 +1,17 @@
 package com.example.there.aroundmenow.placedetails.info
 
 
+import android.os.Bundle
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.there.aroundmenow.R
 import com.example.there.aroundmenow.base.architecture.view.RxFragment
+import com.example.there.aroundmenow.base.architecture.view.ViewDataState
 import com.example.there.aroundmenow.placedetails.PlaceDetailsState
+import com.example.there.aroundmenow.util.ext.placeTypesNames
+import io.reactivex.Observable
+import kotlinx.android.synthetic.main.fragment_place_info.*
 
 
 class PlaceInfoFragment : RxFragment.Stateless.HostAware.WithLayout<Unit, PlaceDetailsState>(
@@ -11,4 +19,14 @@ class PlaceInfoFragment : RxFragment.Stateless.HostAware.WithLayout<Unit, PlaceD
     HostAwarenessMode.PARENT_FRAGMENT_ONLY
 ) {
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        place_info_types_recycler_view?.layoutManager = LinearLayoutManager(context, RecyclerView.HORIZONTAL, false)
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun Observable<PlaceDetailsState>.observeParentFragment() = map { it.place }.subscribeWithAutoDispose {
+        if (it is ViewDataState.Value) {
+            place_info_types_recycler_view?.adapter = PlaceInfoTypesAdapter(it.value.placeTypesNames)
+        }
+    }
 }
---
+++ app/src/main/java/com/example/there/aroundmenow/placedetails/info/PlaceInfoTypesAdapter.kt
@@ -0,0 +1,33 @@
+package com.example.there.aroundmenow.placedetails.info
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.example.there.aroundmenow.R
+import com.example.there.aroundmenow.databinding.PlaceInfoTypeItemBinding
+
+class PlaceInfoTypesAdapter(
+    private val placeTypesNames: List<String>
+) : RecyclerView.Adapter<PlaceInfoTypesAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ViewHolder = ViewHolder(
+        DataBindingUtil.inflate(
+            LayoutInflater.from(parent.context),
+            R.layout.place_info_type_item,
+            parent,
+            false
+        )
+    )
+
+    override fun getItemCount(): Int = placeTypesNames.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.binding.name = placeTypesNames[position]
+    }
+
+    class ViewHolder(val binding: PlaceInfoTypeItemBinding) : RecyclerView.ViewHolder(binding.root)
+}
